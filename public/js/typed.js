8656469ff1d4e23c0be74dcb1b491c1745ed2c9f
ANDREW <66812117+Team-AG@users.noreply.github.com>
2021-02-12 04:26:30 -0500

    Add files via upload

---
+++ public/js/typed.js
@@ -0,0 +1,8 @@
+export default function mainTextAnimation() {
+    return new Typed(".textStyle", {
+        strings: ["US Marine", "Coder", "Hero", "Web Designer", "Father", "Discipline", "Photographer", "Veteran", "Warrior", "Leader", "Designer", "Graduate", "Valuable"],
+        typeSpeed: 60,
+        backSpeed: 100,
+        loop: true,
+    });
+}
